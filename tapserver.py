8e25499ba4a63b3ee82d77bab15a61fe6b7e3da8
Daniel Sparrow <sparrowd@cardiff.ac.uk>
2019-11-27 03:37:20 +0000

    Added signup route to server.py

--- tapserver.py
+++ tapserver.py
@@ -90,5 +90,9 @@ def logoutRoute():
         flash(str(result), "error")
         return redirect(url_for("homePage"))
 
+@app.route("/signup")
+def signupPage():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
